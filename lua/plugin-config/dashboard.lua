e7233a2fa055af161984a11eeab9f48d335830ac
Yjsmall <1186907891@qq.com>
2023-01-04 19:28:36 +0800

    add files

---
+++ lua/plugin-config/dashboard.lua
@@ -0,0 +1,68 @@
+local status, db = pcall(require, "dashboard")
+if not status then
+  vim.notify("没有找到 dashboard")
+  return
+end
+
+db.custom_footer = {
+  "",
+  "",
+  "https://github.com/nshen/learn-neovim-lua",
+}
+
+db.custom_center = {
+  {
+    icon = "  ",
+    desc = "Projects                            ",
+    action = "Telescope projects",
+  },
+  {
+    icon = "  ",
+    desc = "Recently files                      ",
+    action = "Telescope oldfiles",
+  },
+  {
+    icon = "  ",
+    desc = "Edit keybindings                    ",
+    action = "edit ~/.config/nvim/lua/keybindings.lua",
+  },
+  {
+    icon = "  ",
+    desc = "Edit Projects                       ",
+    action = "edit ~/.local/share/nvim/project_nvim/project_history",
+  },
+  -- {
+  --   icon = "  ",
+  --   desc = "Edit .bashrc                        ",
+  --   action = "edit ~/.bashrc",
+  -- },
+  -- {
+  --   icon = "  ",
+  --   desc = "Change colorscheme                  ",
+  --   action = "ChangeColorScheme",
+  -- },
+  -- {
+  --   icon = "  ",
+  --   desc = "Edit init.lua                       ",
+  --   action = "edit ~/.config/nvim/init.lua",
+  -- },
+  -- {
+  --   icon = "  ",
+  --   desc = "Find file                           ",
+  --   action = "Telescope find_files",
+  -- },
+  -- {
+  --   icon = "  ",
+  --   desc = "Find text                           ",
+  --   action = "Telescopecope live_grep",
+  -- },
+}
+
+db.custom_header = {
+  [[███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗]],
+  [[████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║]],
+  [[██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║]],
+ [[ ██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║']],
+  [[██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║]],
+  [[╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝]],
+}
